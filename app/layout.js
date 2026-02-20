a70914b881204b8490c682d64797e6515a0f03a6
Jashanpreet Singh <jashan0802@gmail.com>
2026-02-20 18:36:36 +0530

    metadata

--- app/layout.js
+++ app/layout.js
@@ -9,18 +9,7 @@ export const metadata = {
   title: "Website With Robin",
   description:
     "We build high-converting websites that generate enquiries and sales. Strategy, design, and copy engineered to turn traffic into clients",
-  openGraph: {
-    title: "Domicon",
-    description:
-      "Master the exact systems elite creators use to scale fast and monetize their brands without chasing trends.",
-    images: ["/opengraph-image.png"],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Domicon",
-    description:
-      "Master the exact systems elite creators use to scale fast and monetize their brands without chasing trends.",
-    images: ["/twitter-image.png"],
+  
   },
 };
 
